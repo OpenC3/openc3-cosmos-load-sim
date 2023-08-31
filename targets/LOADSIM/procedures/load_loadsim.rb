601c1fdb13161ec7f388025d8695e71df5d0eec1
Jason Thomas <jason@openc3.com>
2023-08-31 14:22:30 -0600

    Update load_loadsim script

--- targets/LOADSIM/procedures/load_loadsim.rb
+++ targets/LOADSIM/procedures/load_loadsim.rb
@@ -1,4 +1,6 @@
-ENV['OPENC3_API_PASSWORD'] = 'password'
+# ENV['OPENC3_API_PASSWORD'] = 'password'
+ENV['OPENC3_API_USER'] = 'admin'
+ENV['OPENC3_API_PASSWORD'] = 'admin'
 $api_server.instance_variable_get(:@json_drb).instance_variable_set(:@authentication, $api_server.generate_auth())
 
 def install_plugin(gem_name:, variables:, plugin_txt_lines: nil, scope: $openc3_scope)
@@ -8,9 +10,9 @@ def install_plugin(gem_name:, variables:, plugin_txt_lines: nil, scope: $openc3_
     "name": gem_name,
     "variables": variables}
   plugin_hash["plugin_txt_lines"] = plugin_txt_lines if plugin_txt_lines
-  response = $api_server.request('post', endpoint, data: { "plugin_hash" => plugin_hash.to_json }, scope: scope)
+  response = $api_server.request('post', endpoint, data: { plugin_hash: plugin_hash.to_json }, json: true, scope: scope)
   if response.nil? || response.code != 200
-    raise "Failed to install plugin #{gem_name}"
+    raise "Failed to install plugin #{gem_name} with code #{response.code}"
   end
 end
 
@@ -18,20 +20,16 @@ def install_loadsim(target_name, gem_version, scope: $openc3_scope)
   gem_name = "openc3-load-sim-#{gem_version}.gem"
   variables = {
     "sim_target_name": target_name,
-    "num_tlm_packets": "100",
-    "num_tlm_items_per_packet": "100",
+    "num_tlm_packets": "10",
+    "num_tlm_items_per_packet": "20",
     "num_tlm_derived_items_per_packet": "5",
-    "num_tlm_bytes_per_packet": "200",
+    "num_tlm_bytes_per_packet": "80",
     "num_each_tlm_packet_per_sec": "1",
-    "num_cmd_packets": "100",
+    "num_cmd_packets": "10",
     "num_cmd_items_per_packet": "20",
     "num_cmd_derived_items_per_packet": "5",
-    "num_cmd_bytes_per_packet": "100"}
+    "num_cmd_bytes_per_packet": "80"}
   plugin_txt_lines = [
-    "# Note: This plugin includes 4 targets in one plugin to make it easy to install",
-    "# the demo with one plugin.  Generally it is better to only have one",
-    "# target per plugin",
-    "",
     "VARIABLE sim_target_name LOADSIM",
     "VARIABLE num_tlm_packets 10",
     "VARIABLE num_tlm_items_per_packet 1000",
@@ -43,13 +41,17 @@ def install_loadsim(target_name, gem_version, scope: $openc3_scope)
     "VARIABLE num_cmd_derived_items_per_packet 5",
     "VARIABLE num_cmd_bytes_per_packet 100",
     "",
-    "TARGET LOADSIM <%= sim_target_name %>",
+    "TARGET LOADSIM #{target_name}",
     "  TLM_BUFFER_DEPTH 200",
     "",
-    "INTERFACE INT_<%= sim_target_name %> simulated_target_interface.rb sim_loadsim.rb",
-    "  MAP_TARGET <%= sim_target_name %>"]
+    "INTERFACE INT_#{target_name} simulated_target_interface.rb sim_loadsim.rb",
+    "  MAP_TARGET #{target_name}"]
 
   install_plugin(gem_name: gem_name, variables: variables, plugin_txt_lines: plugin_txt_lines, scope: scope)
 end
 
-install_loadsim("LOADSIM3", "1.0.0")
+x = 1
+# (1..10).each do |x|
+  install_loadsim("LOADSIM#{x}", "1.0.0")
+  wait 5
+# end
